145f65946a1712424828d58d18ad45144c48669f
Sinan Can <Sinan51can@gmail.com>
2024-01-30 12:10:07 +0300

    9- Global Hata Yönetimi 9.1- ErrorDetails Sınıfı oluşturuldu. 9.2- UseExceptionHandler metodu (İstisna yönetimi ekledik.) 9.3- Program.cs te ConfigureExceptionHandler() metodunu yapılandırdık. 9.4- try catch ler artık middleware den geçeceği için Controllerdan kaldırdık. 9.5- BookController ve Book Manager sınıfları için Refactoring yapıldı.

---
+++ Entities/Exceptions/BookNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class BookNotFoundException : NotFoundException // sealed olduğu için hiç bir şekilde bir kalıtım işlemi yapılamaz.
+    {
+        public BookNotFoundException(int id) : base($"The book with id : {id} could not found.")
+        {
+        }
+    }
+}
