b49ebe9e39a7e9a1a929df2ab3605dbd3aac1209
Sinan Can <Sinan51can@gmail.com>
2024-02-12 11:52:22 +0300

    23 - Versiyonlama (Versioning) 23.1 - Microsoft.AspNetCore.Mvc.Versioning Pakedi kuruldu 23.2 - BooksV2Controller oluşturuldu. 23.3 - URL ile Versiyonlama yapıldı 23.4 - Header ile Versiyonlama yapıldı 23.5 - Kullanımdan kaldırmayı düşündüğümüz Api için (Header üzerinde) Attribute üzerinde deprecated işlemi yapıldı. 23.6 - Attribüte ten versiyonu kaldırıp (extension metotta) conventions işlemi yapıldı.

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -54,6 +54,12 @@ namespace Services
             return (linkResponse: links, metaData: booksWithMetaData.MetaData);
         }
 
+        public async Task<List<Book>> GetAllBooksAsync(bool trackChanges)
+        {
+            var books = await _manager.Book.GetAllBooksAsync(trackChanges);
+            return books;
+        }
+
         public async Task<BookDto> GetOneBookAsync(int id, bool trackChanges)
         {
             var book = await GetOneBookByIdAnCheckExists(id, trackChanges);
